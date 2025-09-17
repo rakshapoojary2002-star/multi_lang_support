1a21ac5d82cfdbd8391c882dd02e787bba746b2c
Raksha <raksha.poojary2002@gmail.com>
2025-09-17 12:00:30 +0530

    Minor changes

--- my_localization/lib/my_localization.dart
+++ my_localization/lib/my_localization.dart
@@ -1,6 +1,3 @@
 library my_localization;
 
-export 'src/localization/localization.dart';
-
-//Now your package users can just:
-//import 'package:my_localization/my_localization.dart';
+export 'l10n/app_localizations.dart';
